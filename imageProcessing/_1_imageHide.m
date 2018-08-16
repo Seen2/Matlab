7a8a412c9c54579a262550e988b7a58ab54c832e
seen2 <tiwarietc@gmail.com>
2018-08-16 21:52:22 +0530

    Working on Image Processing

--- imageProcessing/_1_imageHide.m
+++ imageProcessing/_1_imageHide.m
@@ -24,14 +24,28 @@ for i=1:m
   end
 end
 
+alpha3=input('enter the value of alpha:');
+
+for i=1:m
+  for j=1:n
+    cl2(i,j)=(1-alpha3)*a(i,j) + alpha3*b(i,j);
+  end
+end
+
+
+
+
 figure;
 
-subplot(2,2,1),imshow(a) , title('Image 1')
+subplot(3,2,1),imshow(a) , title('Image 1')
 
-subplot(2,2,2),imshow(b) , title('Image 2')
+subplot(3,2,2),imshow(b) , title('Image 2')
 
-subplot(2,2,3),imshow(cl) , title('Blended Image 1')
+subplot(3,2,3),imshow(cl) , title('Blended Image 1')
 xlabel(sprintf('alpha value is %g',alpha))
 
-subplot(2,2,4),imshow(cl1) , title('Blended Image 2')
+subplot(3,2,4),imshow(cl1) , title('Blended Image 2')
 xlabel(sprintf('alpha value is %g',alpha2))
+
+subplot(3,2,5),imshow(cl2) , title('Blended Image 3')
+xlabel(sprintf('alpha value is %g',alpha3))
