77b4192741c7cd315c7e0406e974c02dd20901a8
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2018-08-23 12:46:16 +0530

    Update 9-twiddleFactorVarificatiom.m

--- 9-twiddleFactorVarificatiom.m
+++ 9-twiddleFactorVarificatiom.m
@@ -11,3 +11,14 @@ K=input('enter the value for K:');
 N=input('enter the value for N:');
 LHS=exp((-1)*i*2*pi*(K/N))
 RHS=exp((-1)*i*2*pi*((K+N/2)/N))
+
+%unknown
+
+K=input('enter the value for K:');
+N=input('enter the value for N:');
+n=input('enter the value for n:');
+LHS=exp((-1)*i*2*pi*((K*n)/N))
+RHS=exp((-1)*i*2*pi*(K/(N/n)))
+
+
+
