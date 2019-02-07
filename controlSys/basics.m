c53e38d7b4c08c065cba66aadd749f558968336c
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-02-07 15:57:14 +0530

    Updated basics

---
+++ controlSys/basics.m
@@ -0,0 +1,24 @@
+x=[1:0.01:100]
+x=[1:10]
+x={1 2 3 4; 1 5 6 8;1 1 1 1} // matrix
+x={1 2 3 4}
+
+%normal operator are for matrix operations and .operator are for array operations.
+x=[1:.5:50] % genrates array with 0.5 increment.
+plot(x,y) % will plot graph
+y=[1 2 0 4; 1 5 6 8;1 1 1 0; 1 2 3 4]
+det(y) % will calculate detarminant of y
+rank(y) % will calculate rank of y
+x=[3 0 2 1;1 2 3 4; 1 5 6 8;1 1 1 1]
+z=x+y % will make matrix addition
+z=x-y % will make matrix subtraction
+
+% assignment of 07/02/2019
+%1 represent an array with 0 to 1000 with 0.5 incremental.
+%2 represent the tan grapgh and cos graph using matlab.
+%3 represent a 6 by 6 matrix and determine its determinant.
+%4 represent an equation y=sin(x) + cos(x) and show graph using plot().
+%5 represent the equation y=(e^0.02x)*sin(0.002x) and plot the graph.
+%6 represent the equation y=(x^2)sin(x^2) and plot the graph.
+
+
