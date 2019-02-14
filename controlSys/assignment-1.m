3e8daf5d77e7c490c555a9c5159c29653a9f8c0c
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-02-14 15:47:48 +0530

    created assignment1

---
+++ controlSys/assignment-1.m
@@ -0,0 +1,109 @@
+%assignment on 14/02/19
+
+%1 develope the transfer function by using tf func. in matlab
+num=[1 2 3 4]
+den=[2 6 10 12]
+tf(num,den)
+
+%2 2nd order as per as choice
+
+num=[1 2 6]
+den=[2 3]
+tf(num,den)
+
+
+num=[0 3 6]
+den=[2 3]
+tf(num,den)
+
+
+%3 3rd order as per as choice
+
+num=[1 2 6 8]
+den=[2 3 4]
+tf(num,den)
+
+num=[0 7 6 8]
+den=[9 3 4]
+tf(num,den)
+
+num=[0 3 6 9]
+den=[9 3 4]
+tf(num,den)
+
+
+%4 4th order as per as choice
+
+num=[9 1 2 6 8]
+den=[2 5 3 4]
+tf(num,den)
+
+num=[0 7 67 60 80]
+den=[9 30 0 4]
+tf(num,den)
+
+num=[100 300 600 900 1000]
+den=[900 300 400]
+tf(num,den)
+
+num=[200 7800 6100 9100 11000]
+den=[900 300 400]
+tf(num,den)
+
+
+%5 5th order as per as choice
+
+num=[12 9 1 2 6 8]
+den=[2 3 5 3 4]
+tf(num,den)
+
+num=[20 10 7 67 60 80]
+den=[9 30 0 4]
+tf(num,den)
+
+num=[10 20 30 60 90 100]
+den=[900 300 400]
+tf(num,den)
+
+num=[200 7800 6100 1220 9100 11000]
+den=[900 300 400]
+tf(num,den)
+
+num=[12 11 0 9 1 2 6 8]
+den=[2 3 5 3 4]
+tf(num,den)
+
+
+
+%6 6th order as per as choice
+
+
+num=[123 12 9 1 2 6 8]
+den=[2 3 5 3 4]
+tf(num,den)
+
+num=[20 100 700 67 60 80]
+den=[9 30 0 40]
+tf(num,den)
+
+num=[10 20 30 50 60 90 100]
+den=[900 300 400]
+tf(num,den)
+
+num=[200 7800 1100 6100 1220 9100 11000]
+den=[900 300 400]
+tf(num,den)
+
+num=[12 11 0 9 1 2 7 6 8]
+den=[2 3 5 3 4]
+tf(num,den)
+
+
+
+%7 12th order as per as choice
+
+num=[12 22 33 44 55 66 12 11 0 9 1 2 7 6 8]
+den=[2 3 5 3 4]
+tf(num,den)
+
+
