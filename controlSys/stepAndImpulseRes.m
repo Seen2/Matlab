c72e31c3774925a0636468d3d5e67a7aa3ba19f2
Lokpati Tiwari <36004515+Seen2@users.noreply.github.com>
2019-02-28 15:22:03 +0530

    updated

--- controlSys/stepAndImpulseRes.m
+++ controlSys/stepAndImpulseRes.m
@@ -1,3 +1,4 @@
+% date 28/02/2019.
 %consider a system with natural freq. =3 and damping cofficient are 0.7,1,2 . Determine the TF 
 %and representit in matlab.
 %Determine all all the poles and zero by pole-zero plot.
@@ -9,8 +10,12 @@ den=[1 0.8*(zie) 8]
 
 sys=tf(num,den) % transfer function.
 
+%pzplot(sys)
+hold on
 step(sys) % step response of transfer function.
 hold on  % hold the previous plot.
 title('Step and Impulse response.')
 impulse(sys) % impulse response of transfer function.
+
+
 
